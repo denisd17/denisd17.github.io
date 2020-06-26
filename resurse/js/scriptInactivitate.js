52902e09da4d6e2274b8100adfab09a856564dda
Daineanu Denis <61796158+denisd17@users.noreply.github.com>
2020-06-27 02:05:05 +0300

    User Inactiv

---
+++ resurse/js/scriptInactivitate.js
@@ -0,0 +1,56 @@
+window.addEventListener('load', userInactiv)
+
+function userInactiv(){
+	var timpInactivitate = 0;
+	var chenar = document.getElementById("id_inactivitate");
+
+	setInterval(calculTimp, 1000);
+	
+	//RESETARE TIMP INACTIVITATE
+	function resetareTimp(){
+		
+		timpInactivitate = 0;
+		chenar.classList.add("display_none");
+	}
+	//EVENIMENTE MOUSE
+    document.onmousemove=function(e){
+		
+		resetareTimp();
+	}
+	document.onmousedown=function(e){
+		
+		resetareTimp();
+	}
+	document.onmouseup=function(e){
+		
+		resetareTimp();
+	}
+	document.onwheel=function(e){
+		
+		resetareTimp();
+	}
+	
+	//EVENIMENTE TASTATURA
+    document.onkeypress=function(e){
+        
+		resetareTimp();
+    }
+
+	function calculTimp(){
+    
+	timpInactivitate = timpInactivitate + 1;
+    
+	if (timpInactivitate >= 5) {
+        {
+          let mesaj = chenar.children[0].innerHTML;
+		  
+		  mesaj = mesaj.split(" ");
+		  mesaj[5] = timpInactivitate;
+		  mesaj = mesaj.join(" ");
+		  
+		  chenar.children[0].innerHTML = mesaj;
+          chenar.classList.remove("display_none");
+        }
+    }
+}
+}
